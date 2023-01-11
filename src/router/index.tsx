e3b50ff1e3e04f0d542c6bc444c9245d4119d65d
Edvard <edvard.edvardd@bk.ru>
2023-01-11 08:52:01 -0800

    first commit

---
+++ src/router/index.tsx
@@ -0,0 +1,10 @@
+import {createBrowserRouter, createRoutesFromElements} from "react-router-dom";
+import Dashboard from "../components/Dashboard";
+
+const routes = createRoutesFromElements(
+    <Route></Route>
+)
+
+const router = createBrowserRouter(routes)
+
+export default router
